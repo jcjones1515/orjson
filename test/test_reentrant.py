fac827ece4c0e69581c7a12b171e610ba862f6d7
Jason Jones <jason@gaincompliance.com>
2024-02-03 13:56:23 -0600

    test case for reentrant 100% CPU spin

--- test/test_reentrant.py
+++ test/test_reentrant.py
@@ -1,3 +1,6 @@
+import decimal
+
+import pytest
 import orjson
 
 
@@ -14,3 +17,22 @@ def test_reentrant():
     del c
 
     orjson.loads("[" + "[]," * 1000 + "[]]")
+
+
+def test_reentrant_via_exception():
+    def another_encoder(o):
+        # do something but ultimately
+        raise TypeError("bad")
+
+    def default_f(o):
+        try:
+            if type(o) == set:
+                return list(o)
+            else:
+                another_encoder(o)
+        except TypeError as t:
+            print(f"Caught TypeError for {o}")
+            raise t
+
+    with pytest.raises(orjson.JSONEncodeError):
+        orjson.dumps([[{decimal.Decimal("0.0842389659712649442845")}] * 1000], default=default_f)
